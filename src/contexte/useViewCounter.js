6cf6de25c142e2f495f23b052a2fa1a140166495
Gabriel Nyangwile <gabriel.nyangwile@gmail.com>
2025-01-19 15:54:15 +0100

    Add useViewCounter.js

---
+++ src/contexte/useViewCounter.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { doc, updateDoc, increment } from 'firebase/firestore';
+import { db } from '../config/firebase';
+
+const useViewCounter = (blogId) => {
+  useEffect(() => {
+    const incrementViews = async () => {
+      if (!blogId) return;
+      const blogRef = doc(db, 'blogs', blogId);
+      try {
+        await updateDoc(blogRef, {
+          views: increment(1),
+        });
+      } catch (error) {
+        console.error("Erreur d'incrémentation des vues:", error);
+      }
+    };
+    incrementViews();
+  }, [blogId]);
+};
+
+export default useViewCounter;
